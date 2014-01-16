b647ea7b2a379e50bdeb107bb7cff508f38bf538
Chris McClaskey <christopher.m.mcclaskey@gmail.com>
2014-01-16 16:12:53 -0700

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChrisMcclaskeyMe::Application.config.secret_key_base = 'cf1dabaea85d7661a85c553440ea6c5f5ef200b3107cac16229a7f9bef200268fcd79025075fdb94da1122dd4afeb18faffd298582ce0e61007021bfe2f7e813'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChrisMcclaskeyMe::Application.config.session_store :cookie_store, key: '_chris_mcclaskey_me_session'
